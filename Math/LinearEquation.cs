22a2b7b90ed0d83aa84fc38fa7ba78b6aab7ff2f
danielbob999 <danielrobinson999@gmail.com>
2019-02-26 09:12:07 +1300

    Moved TackMath into it's own namespace

---
+++ Math/LinearEquation.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TackEngineLib.Math
+{
+    /// <summary>
+    /// Representation of a linear equation (straight line)
+    /// </summary>
+    public struct LinearEquation
+    {
+        private float m_Gradient;
+        private float m_YIntercept;
+
+        /// <summary>
+        /// The gradient of this line
+        /// </summary>
+        public float Gradient
+        {
+            get { return m_Gradient; }
+            set { m_Gradient = value; }
+        }
+
+        /// <summary>
+        /// The y-intercept of this line
+        /// </summary>
+        public float YIntercept
+        {
+            get { return m_YIntercept; }
+            set { m_YIntercept = value; }
+        }
+
+        /// <summary>
+        /// Intializes a new LinearEquation
+        /// </summary>
+        /// <param name="_gradient">The gradient of the line</param>
+        /// <param name="_yIntercept">The y-intercept of the line</param>
+        public LinearEquation(float _gradient = 0, float _yIntercept = 0)
+        {
+            m_Gradient = _gradient;
+            m_YIntercept = _yIntercept;
+        }
+
+        public override string ToString()
+        {
+            return string.Format("y={0}x+{1}", m_Gradient, m_YIntercept);
+        }
+    }
+}
---
+++ Math/TackMath.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using TackEngineLib.Main;
+
+namespace TackEngineLib.Math
+{
+    public static class TackMath
+    {
+        public static int AbsVali(int _val)
+        {
+            if (_val > 0)
+                return _val;
+            else
+                return _val * -1;
+        }
+
+        public static float AbsValf(float _val)
+        {
+            if (_val > 0)
+                return _val;
+            else
+                return _val * -1.0f;
+        }
+
+        /// <summary>
+        /// Converts an angle in degrees to and angle in radians
+        /// </summary>
+        /// <param name="_angle">An angle in degrees</param>
+        /// <returns>The converted angle in radians</returns>
+        public static double DegToRad(float _angle)
+        {
+            return _angle * (System.Math.PI / 180);
+        }
+
+        /// <summary>
+        /// Finds the intersection point of two lines
+        /// </summary>
+        /// <param name="_lineA">The linear equation of the first line</param>
+        /// <param name="_lineB">The linear equation of the second line</param>
+        /// <param name="_intersectPoint">The point at which these two lines intersect. (0, 0) if lines do not intersect</param>
+        /// <returns>True if the lines intersect at a point, false if lines do not intersect</returns>
+        /// <returntype>bool</returntype>
+        public static bool GetLinearIntersectionPoint(LinearEquation _lineA, LinearEquation _lineB, out Vector2f _intersectPoint)
+        {
+            // Get X value from equations.
+            float xValue = (_lineB.YIntercept - _lineA.YIntercept) / (_lineA.Gradient - _lineB.Gradient);
+
+            // Calculate intersection point on the Y axis, using both equations
+            float yPointLineA = _lineA.Gradient * xValue + _lineA.YIntercept;
+            float yPointLineB = _lineB.Gradient * xValue + _lineB.YIntercept;
+
+            // See if both Y points are equal, if so, _lineA and _lineB intersect. If not equal, lines do not intersect
+            if (yPointLineA == yPointLineB)
+            {
+                _intersectPoint = new Vector2f(xValue, yPointLineA);
+                return true;
+            }
+            else
+            {
+                _intersectPoint = new Vector2f(-1, -1);
+                return false;
+            }
+        }
+
+        /// <summary>
+        /// Calculates the LinearEquation of a line given two points
+        /// </summary>
+        /// <param name="_pointA">The first point</param>
+        /// <param name="_pointB">The second point</param>
+        /// <returns>The LinearEquation of the line</returns>
+        /// <returntype>LinearEquation</returntype>
+        public static LinearEquation GetLinearEquationFromPoints(Vector2f _pointA, Vector2f _pointB)
+        {
+            LinearEquation equation = new LinearEquation();
+
+            // Calculate the gradient
+            equation.Gradient = (_pointB.Y - _pointA.Y) / (_pointB.X - _pointA.X);
+
+            // Calculate the y-intercept using the values if _pointA. The values of _pointB can also be used, the answer will be the same
+            equation.YIntercept = _pointA.Y - (equation.Gradient * _pointA.X);
+
+            return equation;
+        }
+    }
+}
