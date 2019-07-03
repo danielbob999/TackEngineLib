0fd5a5ccaca4047fd57bd19f1a1f6761e40112fa
danielbob999 <danielrobinson999@gmail.com>
2019-07-03 18:34:47 +1200

    Added GUI Button

---
+++ GUI/Button.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TackEngineLib.GUI
+{
+    public class Button
+    {
+        private ButtonStyle mButtonStyle;
+        private string mText;
+        public event EventHandler Click;
+
+        public ButtonStyle Style
+        {
+            get { return mButtonStyle; }
+            set { mButtonStyle = value; }
+        }
+
+        /// <summary>
+        /// The text of this Button
+        /// </summary>
+        public string Text
+        {
+            get { return mText; }
+            set { mText = value; }
+        }
+
+        /// <summary>
+        /// Intialises a new Button
+        /// </summary>
+        public Button() {
+            mText = "Button";
+        }
+
+        public void Update() {
+            if (TackEngineLib.Input.TackInput.MouseButtonDown(Input.MouseButtonKey.Left)) {
+                TackEngineLib.Main.Vector2f mouseVec = TackEngineLib.Input.TackInput.MousePosition();
+                
+                if (mouseVec.X > mButtonStyle.Shape.X && mouseVec.X < (mButtonStyle.Shape.X + mButtonStyle.Shape.Width)) {
+                    if (mouseVec.Y > mButtonStyle.Shape.Y && mouseVec.Y < (mButtonStyle.Shape.Y + mButtonStyle.Shape.Height)) {
+                        if (Click.GetInvocationList().Length > 0) {
+                            Click.Invoke(this, EventArgs.Empty);
+                        }
+                    }
+                }
+            }
+        }
+
+        public void Render() {
+            TackGUI.Box(mButtonStyle.Shape, mButtonStyle.GetBoxStyle());
+            TackGUI.TextArea(mButtonStyle.Shape, mText, mButtonStyle.GetTextAreaStyle());
+        }
+    }
+}
---
+++ GUI/ButtonStyle.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using TackEngineLib.Main;
+
+namespace TackEngineLib.GUI
+{
+    public class ButtonStyle
+    {
+        private float mFontSize;
+        private Colour4b mFontColour;
+        private int mFontFamilyId;
+        private VerticalAlignment mVerticalAlignment;
+        private HorizontalAlignment mHorizontalAlignment;
+        private Colour4b mColour;
+        private GUIBorder mBorder;
+        private Sprite mSpriteTexture;
+        private RectangleShape mShape;
+
+        public RectangleShape Shape
+        {
+            get { return mShape; }
+            set { mShape = value; }
+        }
+
+        public float FontSize
+        {
+            get { return mFontSize; }
+            set { mFontSize = value; }
+        }
+
+        public Colour4b FontColour
+        {
+            get { return mFontColour; }
+            set { mFontColour = value; }
+        }
+
+        public Colour4b BackgroundColour
+        {
+            get { return mColour; }
+            set { mColour = value; }
+        }
+
+        public GUIBorder Border
+        {
+            get { return mBorder; }
+            set { mBorder = value; }
+        }
+
+        public Sprite SpriteTexture
+        {
+            get { return mSpriteTexture; }
+            set {
+                mSpriteTexture = value;
+            }
+        }
+
+        public int FontFamilyId
+        {
+            get { return mFontFamilyId; }
+            set { mFontFamilyId = value; }
+        }
+
+        /// <summary>
+        /// The VerticalAlignment of this text
+        /// </summary>
+        public VerticalAlignment VerticalAlignment
+        {
+            get { return mVerticalAlignment; }
+            set { mVerticalAlignment = value; }
+        }
+
+        /// <summary>
+        /// The HorizontalAlignment of this text
+        /// </summary>
+        public HorizontalAlignment HorizontalAlignment
+        {
+            get { return mHorizontalAlignment; }
+            set { mHorizontalAlignment = value; }
+        }
+
+        public ButtonStyle() {
+            mSpriteTexture = Sprite.DefaultSprite;
+
+            mFontSize = 6f;
+            mFontColour = new Colour4b(0, 0, 0, 255);
+            mColour = new Colour4b(255, 255, 255, 255);
+            mBorder = new GUIBorder(0, 0, 0, 0, new Colour4b(0, 0, 0, 255));
+        }
+
+        internal TextAreaStyle GetTextAreaStyle() {
+            TextAreaStyle style = new TextAreaStyle() {
+                FontSize = mFontSize,
+                BackgroundColour = mColour,
+                FontColour = mFontColour,
+                FontFamilyId = mFontFamilyId,
+                 HorizontalAlignment = mHorizontalAlignment,
+                 VerticalAlignment = mVerticalAlignment,
+                 Scrollable = false,
+                 ScrollPosition = 0.0f,
+                 SpriteTexture = mSpriteTexture
+            };
+
+            return style;
+        }
+
+        internal BoxStyle GetBoxStyle() {
+            BoxStyle style = new BoxStyle() {
+                Colour = mColour,
+                Border = mBorder,
+                SpriteTexture = mSpriteTexture
+            };
+
+            return style;
+        }
+    }
+}
