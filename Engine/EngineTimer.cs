6dc9156e0004ea66de44b0f2265ded9001972a6b
danielbob999 <danielrobinson999@gmail.com>
2021-01-15 11:25:49 +1300

    Added an engine timer class to keep track of frame deltas and engine run times

---
+++ Engine/EngineTimer.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TackEngineLib.Engine {
+    public static class EngineTimer {
+        public static double RunTime { get; internal set; }
+        public static double LastCycleTime { get; internal set; }
+    }
+}
